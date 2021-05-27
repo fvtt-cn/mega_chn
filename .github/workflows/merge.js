565dcb045ddb9b303f66a12995cfc0999d8e8651
hmqgg <mitchx7@outlook.com>
2021-05-27 20:42:00 +0800

    Update merge.js

--- .github/workflows/merge.js
+++ .github/workflows/merge.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
-const merge = require('deepmerge')
+const merge = require('deepmerge');
+const yaml = require('js-yaml');
 
 const entries = fs.readdirSync('./chinese/').map(file => {
-    if (file.endsWith('.json')) {
-        return JSON.parse(fs.readFileSync(`./chinese/${file}`, 'utf8'));
+    const filePath = `./chinese/${file}`;
+    if (file.endsWith('.yml')) {
+        return yaml.load(fs.readFileSync(filePath, 'utf8'));
+    } else if (file.endsWith('.json')) {
+        return JSON.parse(fs.readFileSync(filePath, 'utf8'));
     } else return {};
 });
 
