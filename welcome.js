ca9cd86dd1356ee0695869c17f6bcdaf2171db83
hmqgg <mitchx7@outlook.com>
2021-07-01 23:58:43 +0800

    Add welcome dialog

---
+++ welcome.js
@@ -0,0 +1,71 @@
+const MODULE_NAME = "~~~mega_chn";
+const WELCOMED_NAME = "welcomed";
+const LISTVER_NAME = "listver";
+
+const TRANSLATORS = [
+    "hmqgg",
+    "bnp800",
+    "hxqxh",
+    "psedonatural",
+    "白药君"
+];
+const LIST_VERSION = 1;
+
+Hooks.on("init", () => {
+    game.settings.register(MODULE_NAME, WELCOMED_NAME, {
+        name: WELCOMED_NAME,
+        scope: "client",
+        config: false,
+        type: Boolean,
+        default: false
+    });
+    game.settings.register(MODULE_NAME, LISTVER_NAME, {
+        name: LISTVER_NAME,
+        scope: "client",
+        config: false,
+        type: Number,
+        default: LIST_VERSION
+    });
+});
+
+Hooks.on("ready", () => {
+    const welcomed = game.settings.get(MODULE_NAME, WELCOMED_NAME);
+    const listVersion = game.settings.get(MODULE_NAME, LISTVER_NAME);
+
+    if (!welcomed || listVersion < LIST_VERSION) {
+        game.settings.set(MODULE_NAME, LISTVER_NAME, LIST_VERSION);
+
+        let list = "";
+        for (let tr of TRANSLATORS) {
+            list += `<li>${tr}</li>`;
+        }
+
+        let content = `
+            <h2>欢迎使用 FVTT 中文翻译 Mega 整合包！</h2>
+            <p>Mega 包内置了多个系统和数十个不同 MOD 的海量翻译，适合各类 GM 和玩家使用。</p>
+            <h3>此翻译包的诞生离不开所有译者的支持，特别鸣谢！</h3>
+            <ul>
+                ${list}
+            </ul>
+            <p><small>此外，感谢 <a href="https://weblate.org/">Weblate</a> 项目为此项目提供翻译流程规范化的软件支持！</small></p>
+            <p>如需参与 Mega 包翻译工作，或提议支持更多 MOD/系统，请联系 <a href="https://github.com/fvtt-cn/">FVTT-CN 组织</a>！</p>
+        `;
+
+        const welcomeDialog = new Dialog({
+            title: "已开启 MEGA_CHN 翻译整合包！",
+            content: content,
+            buttons: {
+                ok: {
+                    icon: '<i class="fas fa-check-circle"></i>',
+                    label: "OK"
+                },
+                check: {
+                    icon: '<i class="fas fa-thumbs-up"></i>',
+                    label: "不再显示",
+                    callback: () => game.settings.set(MODULE_NAME, WELCOMED_NAME, true)
+                }
+            }
+        });
+        welcomeDialog.render(true);
+    }
+});
